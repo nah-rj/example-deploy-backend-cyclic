9abefd9e5aad38fc368801e0fc17834fc8f4aae6
nah-rj <nah_rj@hotmail.comm>
2023-11-11 14:56:09 -0300

    alteracao

--- src/controllers/home.ts
+++ src/controllers/home.ts
@@ -2,6 +2,6 @@ import { Request, Response } from 'express'
 
 export class HomeController {
 	async handle(req: Request, res: Response) {
-		return res.json('Tudo ceto! A aplicação está funcionando perfeitamente.')
+		return res.json('Tudo ceto!e nada rresolvido!')
 	}
 }
